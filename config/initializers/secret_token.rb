8476dbb881c36e819b003d33726db66526f93eae
rodrigotello <rodrigotello@outlook.com>
2014-02-07 00:18:58 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flowbyfunkalab::Application.config.secret_token = 'ba75e04c59bef5b9f8ad46bba25146e6dcc84328de11ee9660d75fe8c1e0ee39a67fed10e5746bd8cd5fe53ca69f5b0c9e2ff6c90909ae2cba8085994856a8e7'
